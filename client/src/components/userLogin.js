1006be5d5a3cb87755d27065336fd651cb39101c
jeffbfitz383 <144614006+jeffbfitz383@users.noreply.github.com>
2024-03-05 15:27:00 -0700

    Create userLogin.js

---
+++ client/src/components/userLogin.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+function UserLoggin() {
+
+    return(
+        <div>
+            <h1> User Loggin</h1>
+        </div>
+    );
+}
+
+export default UserLoggin;
