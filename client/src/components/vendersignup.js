5853fe9d7bb2e2c99ddb1cf3c9426f05b7078ad4
Jeffrey <jefbfitzp33@outlook.com>
2024-03-05 03:31:57 -0700

    3

---
+++ client/src/components/vendersignup.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Vendersignup = () => {
+    return (
+        <div>
+            <h1>Signup</h1>
+            <form>
+                <formfield>
+                    <label>Enter vender Name  </label>
+                    <input></input><p></p>
+
+                </formfield>
+
+                <formfield>
+                    <label>Enter Password  </label>
+                    <input></input><p></p>
+
+                </formfield>
+
+                <formfield>
+                    <button type="submit">Submit</button>
+                </formfield>
+
+            </form>
+        </div>
+    );
+};
+
+export default Vendersignup;
