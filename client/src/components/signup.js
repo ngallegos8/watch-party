5094c84a99f770b0711e9b24e4fcbf5d58b9baa5
Jeffrey <jefbfitzp33@outlook.com>
2024-03-04 23:53:48 -0700

    how bout now

---
+++ client/src/components/signup.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <div>
+            <h1>Signup</h1>
+            <form>
+                <formfield>
+                    <label>Enter User Name  </label>
+                    <input></input><p></p>
+
+                </formfield>
+
+                <formfield>
+                    <label>Enter Password  </label>
+                    <input></input><p></p>
+
+                </formfield>
+
+                <formfield>
+                    <button type="submit">Submit</button>
+                </formfield>
+
+            </form>
+        </div>
+    );
+};
+
+export default Signup;
+
+/////
